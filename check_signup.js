b0e538531a7dda823fe3a708cf6bf12c6ffca099
avalonsia <johnny654258@gmail.com>
2015-12-07 14:14:22 +0800

    first version

---
+++ check_signup.js
@@ -0,0 +1,59 @@
+$('.ui.dropdown')
+  .dropdown()
+;
+
+$(function(){
+    $('#signup').click(function(){
+
+        $('#bg').css({'display':'block',
+    		'position':'fixed'});
+        $('#pop-div1').css({'display':'block',
+			'position':'fixed'});
+        $('#pop-div2').css({'display':'none'});
+    });
+    $('#bg').click(function(){
+        $('#bg').css({'display':'none'});
+        $('#pop-div1').css({'display':'none'});
+    });
+});
+
+
+$(function(){
+    $('#signin').click(function(){
+
+        $('#bg').css({'display':'block',
+    		'position':'fixed'});
+
+        $('#pop-div2').css({'display':'block',
+			'position':'absolute'});
+        
+        $('#pop-div1').css({'display':'none'});
+    });
+    $('#bg').click(function(){
+        $('#bg').css({'display':'none'});
+        $('#pop-div2').css({'display':'none'});
+    });
+});
+
+$('#create').click(function(e){
+    e.preventDefault();
+    $.post('/api/sheet/create');
+});
+
+$('#fbsignin').click(function(){
+	window.open('/auth/facebook', 'Login...','width=400,height=200,resizeable,scrollbars');
+});
+
+$('#googlesignin').click(function(){
+	window.open('/auth/google', 'Login...','width=400,height=200,resizeable,scrollbars');
+});
+
+$('#email-auth-ajax').submit(function(e){
+    e.preventDefault();
+	$.post('/auth/email/json/');/*token的部分還要補*/
+});
+
+$('#email-register').submit(function(e){
+    e.preventDefault();
+    $.post('/register/email');
+});
